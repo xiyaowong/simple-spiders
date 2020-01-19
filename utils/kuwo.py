bc67f24f16b5e269cbf3ef91c04ca608a7021ecc
wongxy <xiyao.wong@foxmail.com>
2020-01-19 11:56:01 +0800

    酷我音乐

--- utils/kuwo.py
+++ utils/kuwo.py
@@ -17,7 +17,7 @@ HEADERS = {
     'Connection': 'keep-alive',
     'Cookie': '_ga=GA1.2.1516764754.1579334008; _gid=GA1.2.8717020.1579334008; _gat=1; Hm_lvt_cdb524f42f0ce19b169a8071123a4797=1579334010; Hm_lpvt_cdb524f42f0ce19b169a8071123a4797=1579334010; kw_token=MJ3PBY3HDNP',
     'Host': 'www.kuwo.cn',
-    'Referer': 'http://www.kuwo.cn/play_detail/65265710',
+    'Referer': 'http://www.kuwo.cn/',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
 }
 
@@ -34,16 +34,15 @@ def get(url: str) -> dict:
     ERROR = {'author': '', 'song_name': '', 'play_url': ''}
 
     # http://www.kuwo.cn/play_detail/65265710
-    id = url[31:][0]
+    id = url[31:]
 
-    song_info_url = 'http://www.kuwo.cn/api/www/music/musicInfo?mid={id}'
+    song_info_url = 'http://m.kuwo.cn/newh5/singles/songinfoandlrc?musicId={id}'
     mp3_url = 'http://www.kuwo.cn/url?format=mp3&rid={id}&response=url&type=convert_url3&br={quality}&from=web'
 
     # 得到最高品质以及歌曲信息
     with requests.get(song_info_url.format(id=id), headers=HEADERS, timeout=33) as rep:
         if rep.status_code == 200:
-            print(rep.json())
-            best_quality = rep.json().get('data').get('songinfo').get('coopFormats')[-1]
+            best_quality = rep.json().get('data').get('songinfo').get('coopFormats')[0]
             author = rep.json().get('data').get('songinfo').get('artist')
             song_name = rep.json().get('data').get('songinfo').get('songName')
         else:
@@ -66,4 +65,4 @@ def get(url: str) -> dict:
 
 
 if __name__ == "__main__":
-    print(get(input("url: ")))
+    print(get(input('url:  ')))
